6239895137f7aac78316f72778353fd8bb5564bd
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-09-20 17:51:37 +0300

    add hero

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,26 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      <div className="flex h-screen">
+        <div className="lg:w-1/4">1</div>
+        <div className="lg:w-2/4 flex items-center justify-center">
+          <div className="w-3/4">
+            <p className="text-center my-4">Hi there👋</p>
+            <h1 className="text-center text-4xl my-4 font-sans">
+              My name is Kenna Taddese and I am a Developer
+            </h1>
+            <p className="text-center my-4 text-gray-400">
+              I possess a great personality, with a sense of humor, and am a
+              hard worker ready to tackle challenges and create new digital
+              experiences.
+            </p>
+            <button className="block mx-auto my-4 bg-lime-400 px-12 py-3 text-black rounded-3xl cursor-pointer">
+              Hire Me
+            </button>
+          </div>
+        </div>
+        <div className="lg:w-1/4">3</div>
+      </div>
       <h1 className="text-center">
         Hello world this is me and I a going to test this FIRST
       </h1>
