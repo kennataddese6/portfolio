a4aa63e9ad274ca139e1bf21a53730b6010aa10f
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-09-20 17:48:57 +0300

    fix navbar

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
-    <div className="sticky top-0 py-8">
-      <nav className="flex  justify-between align-middle border border-gray-600  w-screen py-4 px-8 rounded-3xl mx-auto lg:w-3/4 backdrop-blur-2xl">
+    <div className="fixed top-0 py-8 w-screen">
+      <nav className="flex  justify-between align-middle border border-gray-600  w-screen py-3 px-8  rounded-3xl mx-auto lg:w-3/4 backdrop-blur-2xl">
         <div className="flex items-center">
           <div className="w-2 h-2 bg-lime-400 rounded-full mx-2"></div>
           <div className="text-2xl p-0">Kenna</div>
