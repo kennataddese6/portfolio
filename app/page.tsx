a967d8ec6d2ba7c65cb1b155c48ca8e79f85e659
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-10-18 20:28:23 +0300

    add hero sectiona and install react-icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,33 @@
 import Navbar from "./ui/navbar";
 import Image from "next/image";
-import me from "@/app/me.png";
+import me from "@/app/heroImage.png";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <section className="bg-zinc-800 grid grid-cols-2 rounded-2xl lg:mx-96 mt-6">
-        <div className="px-6 py-16 ">
-          <h1 className="text-4xl font-sans my-4 font-bold">
-            My name is Kenna
-          </h1>
-          <p className="my-4 text-gray-300">
-            I create innovative experiences and bring ideas to life with great{" "}
-            <br />
-            precision to bring high end products
-          </p>
-          <button className="bg-orange-700 py-2 px-8 rounded-md">
-            Hire me
-          </button>
-          <button className="bg-zinc-900 mx-2 py-2 px-2 rounded-md text-zinc-400">
-            Copy Email
-          </button>
+      <section className="bg-zinc-800 grid grid-cols-2 rounded-2xl lg:w-3/4 mx-auto mt-6">
+        <div className="lg:px-4 py-16">
+          <div className="lg:px-4 xl:px-20 py-16 ">
+            <h1 className="text-4xl font-sans my-4 font-bold">
+              My name is Kenna
+            </h1>
+            <p className="my-4  text-gray-300">
+              I create innovative experiences and bring ideas to life with great{" "}
+              precision to bring high end products
+            </p>
+            <button className="bg-orange-700 py-2 px-8 rounded-md">
+              Hire me
+            </button>
+            <button className="bg-zinc-900 mx-2 py-2 px-2 rounded-md text-zinc-400">
+              Copy Email
+            </button>
+          </div>
         </div>
-        <div className="px-6 py-8 "></div>
+        <div className="px-6  flex justify-center">
+          <Image src={me} alt="my Image" width={300} className="xl:w-2/4" />
+        </div>
       </section>
-
-      <section>Hello</section>
-      <section>Hello</section>
     </>
   );
 }
--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <nav className="py-8 flex sticky top-0">
-      <ul className="border border-zinc-800  flex justify-between mx-auto  rounded-3xl w-auto backdrop-blur-md">
+      <ul className="border border-zinc-800 flex justify-between mx-auto  rounded-3xl w-auto backdrop-blur-md">
         <li className="bg-zinc-800 px-12 py-2 m-1 rounded-3xl cursor-pointer text-zinc-200">
           Home
         </li>
