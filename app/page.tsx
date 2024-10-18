d387368d4bbf2b1a95270b4f63f517ddf37141db
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-10-18 19:24:58 +0300

    add hero text

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <section className="bg-zinc-900 grid grid-cols-2 gap-4 lg:mx-96 mt-6">
-        <div className="bg-zinc-800 px-6 py-16 rounded-2xl">
+      <section className="bg-zinc-800 grid grid-cols-2 rounded-2xl lg:mx-96 mt-6">
+        <div className="px-6 py-16 ">
           <h1 className="text-4xl font-sans my-4 font-bold">
             My name is Kenna
           </h1>
@@ -23,7 +23,7 @@ export default function Home() {
             Copy Email
           </button>
         </div>
-        <div className="bg-zinc-800 px-6 py-8 rounded-3xl"></div>
+        <div className="px-6 py-8 "></div>
       </section>
 
       <section>Hello</section>
