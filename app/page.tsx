1768195bd5ff82347cd695a11be5105a88cdc046
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-10-21 19:51:12 +0300

    enahncbe ui

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,13 @@ export default function Home() {
       <BackgroundBeamsWithCollision
         className="from-inherit"
         children={
-          <section className="bg-zinc-800 flex flex-wrap rounded-md  xl:w-9/12">
-            <div className="w-full lg:w-1/2 flex items-center pl-7">
+          <section className="bg-zinc-800 flex flex-wrap rounded-md lg:w-10/12 xl:w-9/12">
+            <div className="w-full lg:w-1/2 flex items-center lg:pl-12 xl:pl-16">
               <div>
                 <h1 className="text-4xl font-sans mb-5 font-bold text-white">
                   Hi, My name is <br /> Kenna
                 </h1>
-                <p className="my-4 text-gray-300 lg:w-11/12 xl:w-9/12">
+                <p className="my-4 text-gray-300 lg:w-11/12 xl:w-8/12">
                   Architecting visionary experiences with precision to transform
                   ideas into exquisite, high-end products.
                 </p>
@@ -34,7 +34,7 @@ export default function Home() {
                 src={me}
                 alt="my Image"
                 width={300}
-                className="xl:w-7/12"
+                className="xl:w-6/12"
               />
             </div>
           </section>
