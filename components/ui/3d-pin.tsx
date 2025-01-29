a838229a3488b463795e129ccea50a8c8bfa3705
kennataddese6 <kennataddese6@gmail.com>
2025-01-29 20:11:46 +0300

    fix hydration error

--- components/ui/3d-pin.tsx
+++ components/ui/3d-pin.tsx
@@ -1,8 +1,8 @@
-"use client";
-import React, { useState } from "react";
-import { motion } from "framer-motion";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
+"use client"
+import React, { useState } from "react"
+import { motion } from "framer-motion"
+import { cn } from "@/lib/utils"
+import Link from "next/link"
 
 export const PinContainer = ({
   children,
@@ -11,28 +11,28 @@ export const PinContainer = ({
   className,
   containerClassName,
 }: {
-  children: React.ReactNode;
-  title?: string;
-  href?: string;
-  className?: string;
-  containerClassName?: string;
+  children: React.ReactNode
+  title?: string
+  href?: string
+  className?: string
+  containerClassName?: string
 }) => {
   const [transform, setTransform] = useState(
-    "translate(-50%,-50%) rotateX(0deg)"
-  );
+    "translate(-50%,-50%) rotateX(0deg)",
+  )
 
   const onMouseEnter = () => {
-    setTransform("translate(-50%,-50%) rotateX(40deg) scale(0.8)");
-  };
+    setTransform("translate(-50%,-50%) rotateX(40deg) scale(0.8)")
+  }
   const onMouseLeave = () => {
-    setTransform("translate(-50%,-50%) rotateX(0deg) scale(1)");
-  };
+    setTransform("translate(-50%,-50%) rotateX(0deg) scale(1)")
+  }
 
   return (
     <Link
       className={cn(
         "relative group/pin z-50  cursor-pointer",
-        containerClassName
+        containerClassName,
       )}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
@@ -56,31 +56,27 @@ export const PinContainer = ({
       </div>
       <PinPerspective title={title} href={href} />
     </Link>
-  );
-};
+  )
+}
 
 export const PinPerspective = ({
   title,
   href,
 }: {
-  title?: string;
-  href?: string;
+  title?: string
+  href?: string
 }) => {
   return (
     <motion.div className="pointer-events-none  w-96 h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
       <div className="w-full h-full -mt-7 flex-none inset-0">
         <div className="absolute top-0 inset-x-0  flex justify-center">
-          <a
-            href={href}
-            target={"_blank"}
-            className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10"
-          >
+          <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10">
             <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5">
               {title}
             </span>
 
             <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover/btn:opacity-40"></span>
-          </a>
+          </div>
         </div>
 
         <div
@@ -162,5 +158,5 @@ export const PinPerspective = ({
         </>
       </div>
     </motion.div>
-  );
-};
+  )
+}
