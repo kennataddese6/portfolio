3220759bc47f8d2996a4fc52c876325c9033dbc5
kennataddese6 <kennataddese6@gmail.com>
2025-01-29 11:04:06 +0300

    add an aimated tooltip

---
+++ app/animated-tooltip/page.tsx
@@ -0,0 +1,51 @@
+"use client"
+import React from "react"
+import { AnimatedTooltip } from "@/components/ui/animated-tooltip"
+const people = [
+  {
+    id: 1,
+    name: "John Doe",
+    designation: "Software Engineer",
+    image: "/image.png",
+  },
+  {
+    id: 2,
+    name: "Robert Johnson",
+    designation: "Product Manager",
+    image: "/image.png",
+  },
+  {
+    id: 3,
+    name: "Jane Smith",
+    designation: "Data Scientist",
+    image: "/image.png",
+  },
+  {
+    id: 4,
+    name: "Emily Davis",
+    designation: "UX Designer",
+    image: "/image.png",
+  },
+  {
+    id: 5,
+    name: "Tyler Durden",
+    designation: "Soap Developer",
+    image: "/image.png",
+  },
+  {
+    id: 6,
+    name: "Dora",
+    designation: "The Explorer",
+    image: "/image.png",
+  },
+]
+
+export default function AnimatedTooltipPreview() {
+  return (
+    <div className="w-full h-full bg-black flex ">
+      <div className="flex flex-row items-center justify-center mb-10 w-full">
+        <AnimatedTooltip items={people} />
+      </div>
+    </div>
+  )
+}
--- components/ui/animated-testimonials.tsx
+++ components/ui/animated-testimonials.tsx
@@ -43,7 +43,7 @@ export const AnimatedTestimonials = ({
     return Math.floor(Math.random() * 21) - 10
   }
   return (
-    <div className="w-full h-full bg-black">
+    <div className="w-full h-full bg-black ">
       <div className="max-w-sm md:max-w-4xl mx-auto antialiased font-sans px-4 md:px-8 lg:px-12 py-20">
         <div className="relative grid grid-cols-1 md:grid-cols-2  gap-20">
           <div>
