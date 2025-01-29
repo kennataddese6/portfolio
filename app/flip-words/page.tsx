44ade715f47c7151523a09a3589386982ace3174
kennataddese6 <kennataddese6@gmail.com>
2025-01-29 11:39:58 +0300

    add flip words

---
+++ app/flip-words/page.tsx
@@ -0,0 +1,18 @@
+import { FlipWords } from "@/components/ui/flip-words"
+import React from "react"
+
+export default function FlipWordsDemo() {
+  const words = ["better", "cute", "beautiful", "modern"]
+
+  return (
+    <div className="bg-black h-full">
+      <div className="h-[40rem] flex justify-center items-center px-4">
+        <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+          Build
+          <FlipWords words={words} /> <br />
+          websites with Aceternity UI
+        </div>
+      </div>
+    </div>
+  )
+}
