e8a6eda2abde9e1342edff1185d3f6b4a3e9127c
kennataddese6 <kennataddese6@gmail.com>
2025-01-30 14:47:23 +0300

    add timelines

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -37,6 +37,13 @@ export default function HomeLayout({
         <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
       ),
     },
+    {
+      name: "timelines",
+      link: "/timelines",
+      icon: (
+        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
+    },
   ]
   return (
     <div className="relative  w-full">
