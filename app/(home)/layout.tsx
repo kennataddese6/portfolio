0a0d8a74f4d84bec4b25e7e6bc0159bd39ef7e93
kennataddese6 <kennataddese6@gmail.com>
2025-02-04 10:14:37 +0300

    add background light

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -51,6 +51,13 @@ export default function HomeLayout({
         <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
       ),
     },
+    {
+      name: "light",
+      link: "/background-light",
+      icon: (
+        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
+    },
   ]
   return (
     <div className="relative  w-full">
