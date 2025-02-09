ab1aab7b080b9116b9f1d1b4b50d6841a88a0618
kennataddese6 <kennataddese6@gmail.com>
2025-02-09 20:13:33 +0300

    add projects page and a background to it

---
+++ app/projects/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="h-screen bg-[url(/mobileherobg.png)] xl:bg-[url(/projectsbg.png)]  w-full bg-no-repeat xl:bg-center">
+      {/* <h1 className="text-white">Hello</h1> */}
+      {/* <h1 className="text-white">Hello</h1> */}
+    </div>
+  )
+}
