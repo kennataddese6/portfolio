1d39c4cff56eb903b264e58308906dd57f3d5aac
kennataddese6 <kennataddese6@gmail.com>
2025-02-11 14:37:37 +0300

    fix margin

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,34 +5,33 @@ import { IoLocationOutline } from "react-icons/io5"
 export default function Page() {
   return (
     <div className=" h-screen bg-[url(/mobileherobg.png)] xl:bg-[url(/contactbg.png)]  w-full bg-no-repeat bg-cover 2xl:bg-y-80px">
-      <div className="flex justify-between pt-36 px-20">
-        <div>
-          <h1 className="text-white text-2xl font-bold">Let&apos;s Talk</h1>
+      <div className="flex justify-between pt-24 md:pt-36 md:px-20">
+        <div className="w-full mx-3">
+          <h1 className="text-white text-2xl font-bold text-center md:text-start">
+            Let&apos;s Talk
+          </h1>
           <input
             type="text"
             placeholder="Your Name"
-            className="mt-6 p-3 w-96 bg-black border-2 border-neutral-800 text-neutral-400  outline-none"
+            className="mt-3 md:mt-6 p-3 w-full md:w-96 bg-black border-2 border-neutral-800 text-neutral-400  outline-none"
           />
           <br />
           <input
             type="text"
             placeholder="Your Email"
-            className="mt-6 p-3 w-96 bg-black border-2 border-neutral-800 text-neutral-400  outline-none"
+            className="mt-3 md:mt-6 p-3 w-full md:w-96 bg-black border-2 border-neutral-800 text-neutral-400  outline-none"
           />
           <br />
           <textarea
             placeholder="Your Thoughts"
-            className="mt-6 p-3 w-96 bg-black border-2 border-neutral-800 text-neutral-400  outline-none min-h-24"
+            className="mt-3 md:mt-6 p-3 w-full md:w-96 bg-black border-2 border-neutral-800 text-neutral-400  outline-none min-h-24"
           />
           <br />
-          <button
-            className="py-3 w-96 text-white mt-6"
-            style={{ backgroundColor: "#0B75A7" }}
-          >
+          <button className="py-3 w-full md:w-96 md:text-white mt-3 md:mt-6 bg-white text-black md:bg-[#0B75A7] ">
             Send
           </button>
         </div>
-        <div className="text-white  flex flex-col items-center">
+        <div className="text-white  md:flex flex-col items-center hidden">
           <FiUserPlus
             className="mt-12 hover:text-blue-600 cursor-pointer"
             fontSize={20}
