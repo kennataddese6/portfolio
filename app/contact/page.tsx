b6fb5def1e745c26d6a585b4809b4e46ad2be5af
kennataddese6 <kennataddese6@gmail.com>
2025-02-11 19:39:34 +0300

    remove twitter link

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -110,7 +110,7 @@ export default function Page() {
               <ModalBody>
                 <PinContainer
                   title="Right here!"
-                  href="https://twitter.com/mannupaaji"
+                  href="https://github.com/kennataddese6/"
                 >
                   <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                     <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
